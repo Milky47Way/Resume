c128a43063d55f5bf4a60373479147a524890125
Milk <145270243+Milky47Way@users.noreply.github.com>
2025-04-19 13:20:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-# from flask import Flask
-#
-# app = Flask(__name__)
-#
-# @app.route("/")
-# def index():
-#     return "Hello, world!"
-#
-# app.run()
 
 import pandas as pd
+import numpy as np
+from flask import Flask, render_template
+import os
+def index():
+    data = pd.read_csv("kamila.csv")
+    photo = "photo.jpg"
+    name = data[data["category"] == "name"]["text"].values[0]
+    position = data[data["category"] == "position"]["text"].values[0]
+    contacts = data[data["category"] == "contacts"][["text","link"]].replace({np.nan:None}).values
+    skills = data[data["category"] == "skills"]["text"].values
+    projects = data[data["category"] == "projects"][["text","link"]].replace({np.nan: None}).values
+    education = data[data["category"] == "education"]["text"].values
+    achievements = data[data["category"] == "achievements"]["text"].values
+    facts = data[data["category"] == "facts"]["text"].values
 
+    return render_template("index.html", name=name, photo=photo, position=position, contacts=contacts, skills=skills, projects=projects, education=education, achievements=achievements, facts=facts)
 
-data = {"a": [1, 2, 3, 4], "b": [1, 2, 3, 4]}
+folder = os.getcwd()
+app = Flask(__name__, template_folder=folder, static_folder=folder)
+app.add_url_rule("/", "(index)", index)
 
-df = pd.DataFrame(data)
-
-df.to_csv("kamila.csv")
+app.run()
